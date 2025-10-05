b3e78fcc605e1a3e25a47d8c6a87d3178de9e036
Arafat <mdarafat3167@gmail.com>
2025-10-05 19:37:01 +0600

    added navbar and footer with create the router system

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,19 @@
 import { createBrowserRouter } from 'react-router';
+import Root from '../pages/Root/Root';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import Home from '../pages/Home/Home';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello</div>,
+    Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        path: '/',
+        Component: Home,
+      },
+    ],
   },
 ]);
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">404 Page Not Found</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
